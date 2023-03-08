b0c48b40737ea0b230db4235ec3e9c3282f6a592
KlevisonF <silvaklevison11@gmail.com>
2023-03-08 11:54:22 -0300

    Projeto Finalizado

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+import styles from './Footer.module.css'
+
+
+const Footer = () =>{
+    return (
+    
+        <footer className={styles.footer}>
+            <p>
+            <span>React + TS TODO</span> @2023
+            </p>
+         </footer>
+  
+    )
+} 
+export default Footer;
